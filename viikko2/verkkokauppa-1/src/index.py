4f899b90f6d268722d6258a42eeff09165422edc
Timo Laitila <timo.t.laitila@gmail.com>
2021-04-11 00:17:07 +0300

    Exercise Week 2, 10-12

---
+++ viikko2/verkkokauppa-1/src/index.py
@@ -0,0 +1,40 @@
+from kauppa import Kauppa
+from pankki import Pankki
+from varasto import Varasto
+from viitegeneraattori import Viitegeneraattori
+from kirjanpito import Kirjanpito
+
+
+def main():
+
+    # Dependencies injected
+    viitegeneraattori = Viitegeneraattori()
+    kirjanpito = Kirjanpito()
+    varasto = Varasto(kirjanpito)
+    pankki = Pankki(kirjanpito)
+    #kauppa = Kauppa(varasto, pankki, viitegeneraattori)
+    kauppa = Kauppa()
+
+    # kauppa hoitaa yhden asiakkaan kerrallaan seuraavaan tapaan:
+    kauppa.aloita_asiointi()
+    kauppa.lisaa_koriin(1)
+    kauppa.lisaa_koriin(3)
+    kauppa.lisaa_koriin(3)
+    kauppa.poista_korista(1)
+    kauppa.tilimaksu("Pekka Mikkola", "1234-12345")
+
+    # seuraava asiakas
+    kauppa.aloita_asiointi()
+
+    for _ in range(0, 24):
+        kauppa.lisaa_koriin(5)
+
+    kauppa.tilimaksu("Arto Vihavainen", "3425-1652")
+
+    # kirjanpito
+    for tapahtuma in kirjanpito.tapahtumat:
+        print(tapahtuma)
+
+
+if __name__ == "__main__":
+    main()
