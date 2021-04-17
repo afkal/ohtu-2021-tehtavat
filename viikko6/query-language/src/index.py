34ba9cfa891040b0a090a00b5825ae59f02dba38
Timo Laitila <timo.t.laitila@gmail.com>
2021-04-17 09:35:30 +0300

    Week 6, Exercise 2

--- viikko6/query-language/src/index.py
+++ viikko6/query-language/src/index.py
@@ -1,16 +1,22 @@
 from statistics import Statistics
 from player_reader import PlayerReader
-from matchers import And, HasAtLeast, PlaysIn
+from matchers import Not, And, HasAtLeast, HasFewerThan, PlaysIn
 
 def main():
     url = "https://nhlstatisticsforohtu.herokuapp.com/players.txt"
     reader = PlayerReader(url)
     stats = Statistics(reader)
-
+    '''
     matcher = And(
         HasAtLeast(5, "goals"),
         HasAtLeast(5, "assists"),
         PlaysIn("PHI")
+    )
+    '''
+
+    matcher = And(
+        HasFewerThan(1, "goals"),
+        PlaysIn("NYR")
     )
 
     for player in stats.matches(matcher):
--- viikko6/query-language/src/matchers.py
+++ viikko6/query-language/src/matchers.py
@@ -1,14 +1,42 @@
+class Not:
+    def __init__(self, matcher):
+        self._matcher = matcher
+
+    def matches(self, player):
+        if self._matcher.matches(player):
+            return False
+        return True
+
 class And:
     def __init__(self, *matchers):
         self._matchers = matchers
-    
+
     def matches(self, player):
         for matcher in self._matchers:
             if not matcher.matches(player):
                 return False
-        
         return True
 
+class Or:
+    def __init__(self, *matchers):
+        self._matchers = matchers
+
+    def matches(self, player):
+        for matcher in self._matchers:
+            if matcher.matches(player):
+                return True
+        return False
+'''
+class All:
+    def __init__(self, *matchers):
+        self._matchers = matchers
+
+    def matches(self, player):
+        for matcher in self._matchers:
+            if matcher.matches(player):
+                return True
+        return False
+'''
 class PlaysIn:
     def __init__(self, team):
         self._team = team
@@ -25,3 +53,13 @@ class HasAtLeast:
         player_value = getattr(player, self._attr)
 
         return player_value >= self._value
+
+class HasFewerThan:
+    def __init__(self, value, attr):
+        self._value = value
+        self._attr = attr
+
+    def matches(self, player):
+        player_value = getattr(player, self._attr)
+
+        return player_value < self._value
